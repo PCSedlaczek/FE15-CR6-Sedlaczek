badf9b606fd31f16a98bc69df78ed94b3d815839
Petra Christina <p.sedlaczek@drei.at>
2022-02-19 21:37:07 +0100

    Restore app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FootComponent } from './foot/foot.component';
+import { MenuComponent } from './menu/menu.component';
+import { DetailsComponent } from './details/details.component';
+import { OrderComponent } from './order/order.component';
+import { CardComponent } from './card/card.component';
+import { SpecialsComponent } from './specials/specials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent,
+    FootComponent,
+    MenuComponent,
+    DetailsComponent,
+    OrderComponent,
+    CardComponent,
+    SpecialsComponent
   ],
   imports: [
     BrowserModule,
