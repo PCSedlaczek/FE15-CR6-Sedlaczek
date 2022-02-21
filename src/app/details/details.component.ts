c2e1618201f3af13b0e5f4030643118e8d3d7acd
Petra Christina <p.sedlaczek@drei.at>
2022-02-21 08:19:43 +0100

    Update Details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router'; 
+import { CartService } from '../cart.service';
+import { dishes } from '../dishes';
+import { Dishes } from '../dishes';
 
 @Component({
   selector: 'details',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  dish: Dishes = {} as Dishes;
+  id: number = 0;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
+
+  addToCart() {
+    window.alert('Dish has been added to the cart!');
+    this.cartService.addToCart(this.dish);
+    }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['dishID'];
+      this.dish = dishes[this.id];
+    });
   }
 
 }
