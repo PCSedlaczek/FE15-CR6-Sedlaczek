ac6a7dfd81a39e3a3c532512d2a0ecb7ead34ad0
Petra Christina <p.sedlaczek@drei.at>
2022-02-21 08:18:45 +0100

    Update Order component

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Dishes } from '../dishes';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'order',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+  items: Dishes[] = [];
+  checkoutForm = this.fb.group({
+    name: '',
+    address: '',
+    city: ''
+  });
 
-  constructor() { }
+  constructor(private cartService: CartService, private fb: FormBuilder) { }
+
+  clearCart() {
+    window.alert('Your cart has been cleared');
+    this.items = this.cartService.clearCart();
+  }
+  
+   onSubmit() {
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
+    this.items = this.cartService.getItems();
   }
 
 }
