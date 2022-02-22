1848ae42842dc3cfcddebc6ff49bbec82e6b5502
Petra Christina <p.sedlaczek@drei.at>
2022-02-22 23:27:55 +0100

    Fix app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from  './home/home.component';
 import { AboutComponent } from  './about/about.component';
 import { MenuComponent } from  './menu/menu.component';
+import { DetailsComponent } from  './details/details.component';
 import { OrderComponent } from  './order/order.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "about", component: AboutComponent},
-  {path: "order", component: OrderComponent},
   {path: "menu", component: MenuComponent},
-  {path: 'dishes/:dishID', component: MenuComponent}
+  {path: "order", component: OrderComponent},
+  {path: "menu/dishes/:dishID", component: DetailsComponent}
 ];
 
 @NgModule({
